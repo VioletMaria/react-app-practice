671468f6b55d43ecd0120d08576a1552619bee7e
Violetta Szewczyk <szewczykviolet@gmail.com>
2025-07-23 15:06:05 -0500

    Counter Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from 'react'
 import './App.css';
 
+
+
 const App = () => {
+  const [counter,setCounter] = useState(0);
+
   return (
     <div className="App">
-      <h1>Hello, React!</h1>
+      <button onClick={() => setCounter(prevCount => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(prevCount => prevCount + 1)}>+</button>
     </div>
   );
 }
